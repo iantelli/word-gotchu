df4a43e18132c6722fe5071310bd4881a14e94f1
Kiefer Hogg <kieferhogg@gmail.com>
2022-04-26 08:28:41 -0700

    added socket support

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,9 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
 const db = require("../fakedb");
 
 router.get("/", (req, res) => {
-  res.status(200).send({ message: "Hey!"})
+  res.status(200).send({ message: "Hey!"});
 })
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,16 @@
-const app = require('./app.js');
+const http = require("http");
+const socketio = require("socket.io")
 require("dotenv").config();
 
-const port = process.env.PORT || 8080;
-const url = process.env.URL || `http://localhost:${port}/`
+const app = require("./app.js");
+const server = http.createServer(app)
+const io = socketio(server);
 
-app.listen(port, () => console.log(`Server running on port ${port} \nwebsite: ${url}`));
+io.on("connection", (socket) => {
+  console.log("New WS connection")
+})
+
+const PORT = process.env.PORT || 8080;
+const URL = process.env.URL || `http://localhost:${PORT}/`  
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT} \nwebsite: ${URL}`));
