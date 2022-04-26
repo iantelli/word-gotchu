650ffe69d290bae05ae527a93bcc8f2a44fad640
Ian Chao <ichao2@my.bcit.ca>
2022-04-26 13:47:26 -0700

    added colour classes and changed front end behaviour

--- public/js/wordle.js
+++ public/js/wordle.js
@@ -1,9 +1,12 @@
 function startNewGame() {
     document.querySelector("span.correctCharacters").innerHTML = "Correct Characters: "
     document.querySelector("span.incorrectCharacters").innerHTML = "Incorrect Characters: "
-    document.querySelector("span.correctCharacterPlacements").innerHTML = "_ _ _ _ _";
-    document.querySelector("span.totalGuesses").innerHTML = "Total guesses: 0"
+    document.querySelector("span.totalGuesses").innerHTML = "Total guesses: 0";
+    document.querySelectorAll("span.letter").forEach(element => {
+        element.remove();
+    });
 }
+
 
 document.querySelector("button").addEventListener("click", event => {
     event.preventDefault();
@@ -11,14 +14,36 @@ document.querySelector("button").addEventListener("click", event => {
         word: document.querySelector("input.submitWord").value
     })
     .then(response => {
+        let div = document.createElement("div");
+
+        const word = response.data.word;
+
         const correctCharacters = document.querySelector("span.correctCharacters");
         const incorrectCharacters = document.querySelector("span.incorrectCharacters");
         const totalGuesses = document.querySelector("span.totalGuesses");
-        const correctCharacterPlacements = document.querySelector("span.correctCharacterPlacements");
+        const input = document.querySelector("input.submitWord");
         const completed = document.querySelector("span.completed");
-        const word = response.data.word
+        const answerDiv = document.querySelector("div.answerContainer");
+        
+        // Adding colour classes to the correct/incorrect letters
+        (input.value.split("")).forEach((letter, index) => {
+            let span = document.createElement("span");
+            span.classList.add("letter");
+            if (word.correctCharacterPlacements[index] === letter) {
+                span.classList.add("correctCharacterPlacement");
+            }
+            if (word.correctCharacters.includes(letter)) {
+                span.classList.add("correctCharacter");
+            } else if (word.incorrectCharacters.includes(letter)) {
+                span.classList.add("incorrectCharacter");
+            }
+            span.appendChild(document.createTextNode(letter));
+            div.appendChild(span);
+            answerDiv.appendChild(div);
+        }) 
+
         document.querySelector("input.submitWord").value = "";
-        completed.innerHTML = ""
+        completed.innerHTML = "";
         correctCharacters.innerHTML = "Correct Characters: "
         incorrectCharacters.innerHTML = "Incorrect Characters: "
         Object.values(word.correctCharacters).forEach(letter => {
@@ -27,7 +52,6 @@ document.querySelector("button").addEventListener("click", event => {
         Object.values(word.incorrectCharacters).forEach(letter => {
             incorrectCharacters.innerHTML += letter + " ";
         })
-        correctCharacterPlacements.innerHTML = word.correctCharacterPlacements.join(" ");
         totalGuesses.innerHTML = "Total guesses: " + word.totalGuesses;
         if (word.completed) {
             axios.post("/api/v1/lobbyWin", {
@@ -35,6 +59,7 @@ document.querySelector("button").addEventListener("click", event => {
             })
             completed.innerHTML = `correct! The word was ${word.correctCharacterPlacements.join("")}`;
             startNewGame();
+            
         }
         if (word.totalGuesses == 5 && !word.completed) {
             axios.post("/api/v1/lobbyLose", {
