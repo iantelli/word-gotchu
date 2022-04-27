b37dc42685abb5c923a3beba8d9143565a1a3cd6
Sean Ng <sean.ng.cip@gmail.com>
2022-04-27 11:16:07 -0700

    more firebase configs

--- firebase/auth.js
+++ firebase/auth.js
@@ -2,22 +2,10 @@ import { initializeApp } from "firebase/app";
 import { getAuth, onAuthStateChanged, GoogleAuthProvider, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "firebase/auth";
 // https://firebase.google.com/docs/auth/web/manage-users#web-version-9_2
 
-const provider = new GoogleAuthProvider();
+// const provider = new GoogleAuthProvider();
 
-import config from "./config.json";
 import "dotenv/config";
 
-// See: https://firebase.google.com/docs/web/learn-more#config-object
-// const firebaseConfig = {
-//   apiKey: config.apiKey,
-//   authDomain: config.authDomain,
-//   projectId: config.projectId,
-//   storageBucket: config.storageBucket,
-//   messagingSenderId: config.messagingSenderId,
-//   appId: config.appId,
-//   measurementId: config.measurementId,
-//   databaseURL: config.databaseURL
-// };
 
 const firebaseConfig = {
   apiKey: process.env.API_KEY,
@@ -29,6 +17,7 @@ const firebaseConfig = {
   measurementId: process.env.MEASUREMENT_ID,
   databaseURL: process.env.DATABASE_URL
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
