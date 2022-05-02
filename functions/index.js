df3ba8f51da88f6a98ce877147e82ed77054c14e
Sean Ng <sean.ng.cip@gmail.com>
2022-05-02 15:30:48 -0700

    Cloud bugfixing

--- functions/index.js
+++ functions/index.js
@@ -3,6 +3,7 @@ const functions = require("firebase-functions");
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 
@@ -21,11 +22,12 @@ app.use(cors({option: true}));
 
 
 app.get("/expressTest", (req, res) => {
-  res.sendFile("../public/express.html");
+  res.sendFile(path.join(__dirname + "/public/express.html"));
 });
 
 app.get("/id/:id", (req, res) => {
-  res.sendFile("../public/id.html");
+  res.sendFile(path.join(__dirname + "/public/id.html"));
 });
+
 
 exports.app = functions.https.onRequest(app);
