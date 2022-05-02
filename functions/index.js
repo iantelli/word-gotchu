86d1aa95a228b563e2760fab1e75ffa05b1e1f87
Sean Ng <sean.ng.cip@gmail.com>
2022-05-02 12:36:56 -0700

    Cloud functions bugfixing

--- functions/index.js
+++ functions/index.js
@@ -1,56 +1,32 @@
 const functions = require("firebase-functions");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
-
-
 const express = require("express");
 const bodyParser = require("body-parser");
-const cookieSession = require("cookie-session");
 const cors = require("cors");
-
-const router = require("./routes/router");
-const apiRouter = require("./routes/api");
-// const db = require("./fakedb")
 
 const app = express();
 
-app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
-app.use(cookieSession({
-  name: "player",
-  keys: ["cookieMonsterProtection1", "cookieMonsterProtection2"],
-}));
-app.use(express.static("public"));
-
 app.use(cors({option: true}));
 
-app.use("/", router);
-app.use("/api/v1", apiRouter);
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
 
+// exports.helloWorld = functions.https.onRequest((req, res) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   res.send("Hello from Firebase!");
+// });
 
-// Error Handling
-app.use((req, res, next) => {
-  const error = new Error(`Not Found - ${req.originalUrl}`);
-  res.status(404);
-  next(error);
+app.get("/expressTest", (req, res) => {
+  const date = Date.now();
+  res.send(`Hello from Express! ${date}`);
 });
 
-app.use((err, req, res, next) => {
-  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
-  res.status(statusCode);
-  res.json({
-    message: err.message,
-  });
+app.get("/app/:id", (req, res) => {
+  const id = req.params.id;
+  res.send(`Hello from Express! ${id}`);
 });
-
 
 exports.widgets = functions.https.onRequest(app);
-
-// module.exports = app;
