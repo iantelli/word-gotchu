764f0ab884de54642f8295abff6274bdfb2e2331
Sean Ng <sean.ng.cip@gmail.com>
2022-05-03 16:56:04 -0700

    removed m0re dependencies

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,6 @@
-const firebase = require("firebase");
-const functions = require("firebase/functions");
-
-firebase.initializeApp({
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  databaseURL: process.env.DATABASE_URL,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID,
-  measurementId: process.env.MEASUREMENT_ID,
-});
-
-const database = firebase.database();
-const dbRef = database.ref();
+// const firebase = require("firebase");
+// const admin = require("firebase-admin");
+const functions = require("firebase-functions");
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -44,19 +31,4 @@ exports.app = functions.https.onRequest(app);
 exports.helloWorld = functions.https.onRequest((req, res) => {
   functions.logger.info("Hello logs!", {structuredData: true});
   res.send("Hello from Firebase!");
-});
-
-exports.getPlayerById = functions.https.onCall((data, context) => {
-  const id = data.id;
-  dbRef.child("users").child(id).once
-      .then((snapshot) => {
-        if (snapshot.exists()) {
-          return snapshot.val();
-        } else {
-          return null;
-        }
-      })
-      .catch((error) => {
-        console.log(error);
-      });
 });
