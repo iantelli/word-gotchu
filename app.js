2cab31aed2ac6c904c03e7a725933fc70b0fde56
Ian Chao <90526260+iantelli@users.noreply.github.com>
2022-05-05 12:42:56 -0700

    deleting some stuff

--- app.js
+++ app.js
@@ -4,11 +4,9 @@ const cookieSession = require("cookie-session");
 
 const router = require("./routes/router");
 const apiRouter = require("./routes/api")
-const db = require("./fakedb")
 
 const app = express();
 
-app.set("view engine", "ejs")
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json())
 app.use(cookieSession({
