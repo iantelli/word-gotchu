7942d05d42a89025292c1c29ba616b9ab022f2f0
hlee443 <hlee443@my.bcit.ca>
2022-05-05 00:01:04 -0700

    week1 pulled

--- app.js
+++ app.js
@@ -1,15 +1,40 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cookieSession = require('cookie-session');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cookieSession = require("cookie-session");
+
+const router = require("./routes/router");
+const apiRouter = require("./routes/api")
+const db = require("./fakedb")
 
 const app = express();
 
-app.use(express.static("static"))
+app.set("view engine", "ejs")
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
+app.use(cookieSession({
+  name: 'player',
+  keys: ['cookieMonsterProtection1', 'cookieMonsterProtection2']
+}));
+app.use(express.static("public"));
 
-app.set('view engine', 'ejs')
+app.use("/", router);
+app.use("/api/v1", apiRouter)
 
-app.get('/', (req, res) => {
-    res.send("Hello World!")
+
+// Error Handling 
+app.use((req, res, next) => {
+  const error = new Error(`Not Found - ${req.originalUrl}`);
+  res.status(404);
+  next(error);
 });
+
+app.use((err, req, res, next) => {
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  res.status(statusCode);
+  res.json({
+    message: err.message
+  })
+})
+
 
 module.exports = app;
