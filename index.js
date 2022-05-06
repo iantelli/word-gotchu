c62da7b0e94008a79dde5aa0aa6f2d2a0a513784
Ivy Lee <hlee443@my.bcit.ca>
2022-05-05 17:02:05 -0700

    game arena responsive height

---
+++ index.js
@@ -0,0 +1,61 @@
+
+document.addEventListener("click", function (event) {
+    if (event.target.classList.contains("letter")) {
+        event.preventDefault()
+        console.log(event.target.innerHTML)
+        document.querySelector(".output").innerHTML += event.target.innerText
+    } else if (event.target.classList.contains('del')) {
+        document.querySelector(".output").innerHTML = document.querySelector(".output").innerHTML.slice(0, -1)
+
+    }
+    if (event.target.classList.contains('green')) {
+        document.querySelectorAll('.one').forEach(element => {
+            element.className = 'key one green'
+            console.log(element.className)
+
+        });
+
+    } else if (event.target.classList.contains('yellow')) {
+        document.querySelectorAll('.one').forEach(element => {
+            element.className = 'key one yellow'
+            console.log(element.className)
+        });
+    } else if (event.target.classList.contains('black')) {
+        document.querySelectorAll('.one').forEach(element => {
+            element.className = 'key one black'
+            console.log(element.className)
+        });
+    } else if (event.target.classList.contains('default')) {
+        document.querySelectorAll('.one').forEach(element => {
+            element.className = 'key one'
+            console.log(element.className)
+        });
+    }
+
+})
+
+
+
+
+// Change keys on press
+
+const allKeys = [..."abcdefghijklmnopqrstuvwxyz", "enter", "backspace"]
+
+document.addEventListener("keydown", function (event) {
+    const keyPressed = event.key.toLowerCase();
+    if (allKeys.includes(event.key.toLowerCase())) {
+        console.log(event.key.toLowerCase())
+        const key = document.querySelector("#" + keyPressed);
+        key.classList.add("pressed");
+        key.children[0].classList.add("pressed")
+    }
+})
+
+document.addEventListener("keyup", function (event) {
+    const keyPressed = event.key.toLowerCase();
+    if (allKeys.includes(event.key.toLowerCase())) {
+        const key = document.querySelector("#" + keyPressed);
+        key.classList.remove("pressed");
+        key.children[0].classList.remove("pressed")
+    }
+})
--- public/js/index.js
+++ public/js/index.js
@@ -40,21 +40,23 @@ function checkForOtherPlayer() {
 
 intervalID = setInterval(checkForOtherPlayer, 1000)
 
-// const backgroundElement = document.querySelector(".background")
+const backgroundElement = document.querySelector(".background")
 
-// function reScaleWhatever() {
-//   const scale = window.innerWidth/1440
-//   backgroundElement.style.transform = `scale(${scale}`
-//   const y = 1024 * (1029/(backgroundElement.offsetHeight+35))
-//   const backgroundStyle = `url("../images/assets_spritesheet.png") no-repeat -0px -${y}px`
-//   backgroundElement.style.background = backgroundStyle
-//   backgroundElement.style.height = `${window.innerHeight*(1/scale)}px`
-//   console.log(backgroundStyle)
+function reScaleWhatever() {
+  const scaleWidth = window.innerWidth/1440
+  const scaleHeight = window.innerHeight/1024
+  backgroundElement.style.transform = `scale(${scaleWidth,scaleHeight}`
+  const y = 1024 * (1029/(backgroundElement.offsetHeight+35))
+  const backgroundStyle = `url("../images/assets_spritesheet.png") no-repeat -0px -${y}px`
+  backgroundElement.style.background = backgroundStyle
+  backgroundElement.style.width = `${window.innerWidth*(1/scale)}px`
+  console.log(backgroundStyle)
+  console.log((scale-.77))
 
-// }
-// window.addEventListener("resize", event => {
-//   reScaleWhatever()
-// })
+}
+window.addEventListener("resize", event => {
+  reScaleWhatever()
+})
 
-// reScaleWhatever()
+reScaleWhatever()
 
