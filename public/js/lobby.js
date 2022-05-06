bb144c1ff48f2aed39dea31230ea2898b4cb5962
Kiefer Hogg <42953867+Kieferhogg@users.noreply.github.com>
2022-05-06 00:55:59 -0700

    added potatos

--- public/js/lobby.js
+++ public/js/lobby.js
@@ -5,7 +5,7 @@
   let playerRef;
   let lobbyRef;
   let allPlayersRef;
-  let currentWordleRef;
+  let gamStarted = false;
   let lobbyId = window.location.pathname.split("/")[2];
 
   function startNewWordle() {
@@ -26,7 +26,7 @@
   function guessWord(word) {
     return new Promise((resolve, reject) => {
 
-      currentWordleRef = firebase.database().ref(`lobbies/${lobbyId}/players/${playerId}/currentWordle`);
+      let currentWordleRef = firebase.database().ref(`lobbies/${lobbyId}/players/${playerId}/currentWordle`);
       currentWordleRef.get().then((snapshot) => {
         let wordleSnapshot = snapshot.val()
         if (!wordleSnapshot.correctCharacters) wordleSnapshot.correctCharacters = [];
@@ -60,141 +60,147 @@
     })
   }
 
-  document.querySelector("button").addEventListener("click", event => {
-    event.preventDefault();
+  // document.querySelector("button").addEventListener("click", event => {
+  //   event.preventDefault();
 
-    const inputWord = document.querySelector("input.submitWord").value.toLowerCase()
-    if (inputWord.length === 5) {
-      guessWord(inputWord).then((word) => {
-        word.correctCharacters = Array.from(word.correctCharacters);
-        word.incorrectCharacters = Array.from(word.incorrectCharacters);
-        console.log(word)
+  //   const inputWord = document.querySelector("input.submitWord").value.toLowerCase()
+  //   if (inputWord.length === 5) {
+  //     guessWord(inputWord).then((word) => {
+  //       word.correctCharacters = Array.from(word.correctCharacters);
+  //       word.incorrectCharacters = Array.from(word.incorrectCharacters);
+  //       console.log(word)
 
-        let div = document.createElement("div");
-        const correctCharacters = document.querySelector("span.correctCharacters");
-        const incorrectCharacters = document.querySelector("span.incorrectCharacters");
-        const totalGuesses = document.querySelector("span.totalGuesses");
-        const input = document.querySelector("input.submitWord");
-        const completed = document.querySelector("span.completed");
-        const answerDiv = document.querySelector("div.answerContainer");
+  //       let div = document.createElement("div");
+  //       const correctCharacters = document.querySelector("span.correctCharacters");
+  //       const incorrectCharacters = document.querySelector("span.incorrectCharacters");
+  //       const totalGuesses = document.querySelector("span.totalGuesses");
+  //       const input = document.querySelector("input.submitWord");
+  //       const completed = document.querySelector("span.completed");
+  //       const answerDiv = document.querySelector("div.answerContainer");
 
-        // Adding color classes to the correct/incorrect letters
-        (input.value.toLowerCase().split("")).forEach((letter, index) => {
-          let span = document.createElement("span");
-          span.classList.add("letter");
-          if (word.correctCharacterPlacements[index] === letter) {
-            span.classList.add("correctCharacterPlacement");
-            document.querySelector(`#${letter}`).classList.add("green");
-          }
-          else if (word.correctCharacters.includes(letter)) {
-            span.classList.add("correctCharacter");
-            document.querySelector(`#${letter}`).classList.add("yellow");
-          } else if (word.incorrectCharacters.includes(letter)) {
-            span.classList.add("incorrectCharacter");
-            document.querySelector(`#${letter}`).classList.add("black");
-          }
-          div.classList.add("wordleContainer");
-          span.appendChild(document.createTextNode(letter));
-          div.appendChild(span);
-          answerDiv.appendChild(div);
-        })
+  //       // Adding color classes to the correct/incorrect letters
+  //       (input.value.toLowerCase().split("")).forEach((letter, index) => {
+  //         let span = document.createElement("span");
+  //         span.classList.add("letter");
+  //         if (word.correctCharacterPlacements[index] === letter) {
+  //           span.classList.add("correctCharacterPlacement");
+  //           document.querySelector(`#${letter}`).classList.add("green");
+  //         }
+  //         else if (word.correctCharacters.includes(letter)) {
+  //           span.classList.add("correctCharacter");
+  //           document.querySelector(`#${letter}`).classList.add("yellow");
+  //         } else if (word.incorrectCharacters.includes(letter)) {
+  //           span.classList.add("incorrectCharacter");
+  //           document.querySelector(`#${letter}`).classList.add("black");
+  //         }
+  //         div.classList.add("wordleContainer");
+  //         span.appendChild(document.createTextNode(letter));
+  //         div.appendChild(span);
+  //         answerDiv.appendChild(div);
+  //       })
 
-        document.querySelector("input.submitWord").value = "";
-        completed.innerHTML = "";
-        correctCharacters.innerHTML = "Correct Characters: "
-        incorrectCharacters.innerHTML = "Incorrect Characters: "
-        Object.values(word.correctCharacters).forEach(letter => {
-          correctCharacters.innerHTML += letter + " ";
-        })
-        Object.values(word.incorrectCharacters).forEach(letter => {
-          incorrectCharacters.innerHTML += letter + " ";
-        })
-        totalGuesses.innerHTML = "Total guesses: " + word.totalGuesses;
+  //       document.querySelector("input.submitWord").value = "";
+  //       completed.innerHTML = "";
+  //       correctCharacters.innerHTML = "Correct Characters: "
+  //       incorrectCharacters.innerHTML = "Incorrect Characters: "
+  //       Object.values(word.correctCharacters).forEach(letter => {
+  //         correctCharacters.innerHTML += letter + " ";
+  //       })
+  //       Object.values(word.incorrectCharacters).forEach(letter => {
+  //         incorrectCharacters.innerHTML += letter + " ";
+  //       })
+  //       totalGuesses.innerHTML = "Total guesses: " + word.totalGuesses;
 
-        //TODO MOVE TO BACKEND BIG CHEATS
-        if (word.completed) {
-          completed.innerHTML = `correct! The word was ${word.correctCharacterPlacements.join("")}`;
-          playerRef.get().then((snapshot) => {
-            let player = snapshot.val()
-            playerRef.update({
-              score: player.score + 1
-            })
-          })
+  //       //TODO MOVE TO BACKEND BIG CHEATS
+  //       if (word.completed) {
+  //         completed.innerHTML = `correct! The word was ${word.correctCharacterPlacements.join("")}`;
+  //         playerRef.get().then((snapshot) => {
+  //           let player = snapshot.val()
+  //           playerRef.update({
+  //             score: player.score + 1
+  //           })
+  //         })
 
-          startNewWordle();
-        }
-        if (word.totalGuesses === 5 && !word.completed) {
-          playerRef.get().then((snapshot) => {
-            let player = snapshot.val()
-            playerRef.update({
-              score: player.score - 1
-            })
-          })
-          completed.innerHTML = `Too many incorrect guesses! Start a new game!`;
-          startNewWordle();
-        }
-      });
-    }
-  })
+  //         startNewWordle();
+  //       }
+  //       if (word.totalGuesses === 5 && !word.completed) {
+  //         playerRef.get().then((snapshot) => {
+  //           let player = snapshot.val()
+  //           playerRef.update({
+  //             score: player.score - 1
+  //           })
+  //         })
+  //         completed.innerHTML = `Too many incorrect guesses! Start a new game!`;
+  //         startNewWordle();
+  //       }
+  //     });
+  //   }
+  // })
 
   function initGame() {
-    //for lobbies `lobbies/${lobbyId}`
+    allPlayersRef.on("child_changed", (snapshot) => {
+      
+      //On new player
+      const addedPlayer = snapshot.val();
+      if (addedPlayer.num === 2 && !gamStarted) {
+        document.querySelector(`.player${addedPlayer.num}_username`).innerHTML = `Them`;
+        gamStarted = true;
+        startGame();
+      }
+    })
 
-    // currentWordleRef.on("value", (snapshot) => {
-    //   wordle = snapshot.val() || {};
-    // })
-
-    // allPlayersRef.on("value", (snapshot) => {
-    //   //On change
-    //   players = snapshot.val() || {};
-    //   Object.keys(players).forEach((key) => {
-    //     const playerState = players[key];
-    //   })
-
-
-    // })
-
-    // allPlayersRef.on("child_node", (snapshot) => {
-    //   //On new player
-    //   const addedPlayer = snapshot.val();
-    //   if (Object.keys(players).length >= 2) {
-    //     //do something lobby is full
-    //     document.querySelector(".container").innerHTML = "Sorry there are already 2 players in this game."
-    //   } else if (addedPlayer.id !== playerId) {
-    //     startGame();
-    //   }
-    // })
-
-    // allPlayersRef.on("child_removed", (snapshot) => {
-    //   const removedKey = snapshot.val().id;
-    //   document.querySelector(".container").innerHTML = "Opponent Disconnected!"
-    //   delete players[removedKey]
-    // })
+    allPlayersRef.on("child_removed", (snapshot) => {
+      const removedKey = snapshot.val().id;
+      document.querySelector(".container").style = "  justify-content: center; align-items: center; height: 100%; color: black;"
+      document.querySelector(".container").innerHTML = "Opponent Disconnected!"
+    })
   }
 
-  // function startGame() {
-  //   lobbyRef.set({
-  //     startTime: Date.now()
-  //   })
+  function startGame() {
+    const timerElement = document.getElementById("timer");
+    const cd = 5;
+    const time = (1 * 60) + cd;
+    let timer = time;
+    let timerID;
+    let started = false;
 
-  //   let timer = 60;
-  //   let timerID;
+    function secondsToMS(d) {
+      d = Number(d);
+      var m = Math.floor(d % 3600 / 60);
+      var s = Math.floor(d % 3600 % 60);
 
-  //   // add code here to insert the game screen into the lobby cause no ejs
-  //   //have the lobby.ejs to lobby.html file to just be a blank page with Wating for player...
-  //   timerID = setInterval(() => {
-  //     if (timer > 0) {
-  //       timer--;
-  //       document.getElementById("timer").innerHTML = `Time remaining: ${timer} seconds`;
-  //     }
-  //     if (timer <= 0) {
-  //       clearInterval(timerID)
-  //       document.getElementById("timer").innerHTML = "";
-  //       document.querySelector(".container").innerHTML = "Done!"
-  //     }
-  //   }, 1000);
-  //   startNewWordle()
-  // }
+      var mDisplay = m > 0 ? (m < 10 ? `${m}` : m) : "0";
+      var sDisplay = s > 0 ? (s < 10 ? `0${s}` : s) : "00";
+      return `${mDisplay}:${sDisplay}`;
+    }
+
+    timerID = setInterval(() => {
+      if (timer > 0) {
+        timer--;
+        if (!started && timer <= time - cd) {
+          started = true;
+          document.querySelector(".wordle_bars").style = "display: block;";
+          document.querySelector(".keyboard").style = "display: block;";
+        }
+
+        timerElement.innerHTML = secondsToMS(timer)
+      }
+      if (timer <= 0) {
+        clearInterval(timerID)
+        timerElement.innerHTML = "Done!"
+        timerElement.style = "font-size: 21px; left: 516px;"
+        document.querySelector(".wordle_bars").style = "display: none;";
+        document.querySelector(".keyboard").style = "display: none;";
+
+        allPlayersRef.get().then((snapshot) => {
+          let allPlayers = snapshot.val() || {};
+          Object.values(allPlayers).forEach((player) => {
+            document.querySelector(`.player${player.num}_score`).innerHTML = player.score;
+          })
+        })
+      }
+    }, 1000);
+  }
 
   //Login/Logout
   firebase.auth().onAuthStateChanged((user) => {
@@ -217,14 +223,19 @@
       })
 
       allPlayersRef.get().then((snapshot) => {
-        let playerNum = Object.entries(snapshot.val()).length
+        let allPlayers = snapshot.val() || {};
+        let playerNum = Object.entries(allPlayers).length
         playerRef.update({
           num: playerNum
         })
-        document.getElementById("playernum").innerHTML = playerNum;
+
+        document.querySelector(`.player${playerNum}_username`).innerHTML = `Me`;
+        document.querySelector(`.player${playerNum}_username`).style = "color: red;"
+        if (playerNum !== 1) document.querySelector(`.player1_username`).innerHTML = `Them`;
       })
 
       playerRef.onDisconnect().remove();
+      lobbyRef.onDisconnect().remove();
 
       initGame();
     }
