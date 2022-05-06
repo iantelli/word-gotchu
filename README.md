1462935553d7deb6d88ab180898957c2b71a23d4
Ian Chao <90526260+iantelli@users.noreply.github.com>
2022-05-06 01:38:56 -0700

    gigachad

--- README.md
+++
@@ -1,2 +0,0 @@
-# word-gotchu
-Pixel styled web platform game based on Wordle and original characters known as “Gotchus’’ that battle against each other in an arena.
--- server.js
+++
@@ -1,7 +0,0 @@
-require("dotenv").config();
-const app = require('./app.js');
-
-const port = process.env.PORT || 8080;
-const url = process.env.URL || `http://localhost:${port}/`
-
-app.listen(port, () => console.log(`Server running on port ${port} \nwebsite: ${url}`));
