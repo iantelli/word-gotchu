1003734affb9e5008bc9f6a30b1bbf3f55fb60cb
Kiefer Hogg <42953867+Kieferhogg@users.noreply.github.com>
2022-05-12 03:51:57 -0700

    added ivy's stuff

--- routes/router.js
+++ routes/router.js
@@ -29,32 +29,15 @@ router.get("/homepage", (req, res) => {
   const pet = {
     lvl: 1,
     xp: 10,
-    lvl1Story: "QWErty",
-    lvl2Story: "ASDfgh",
-    lvl3Story: "ZXCvbn",
-    lvl1Ability: "QWErty",
-    lvl2Ability: "ASDfgh",
-    lvl3Ability: "ZXCvbn",
+    lvl1Story: "qwerty",
+    lvl2Story: "asdfgh",
+    lvl3Story: "zxcvbn",
   }
   res.render("homepage", { data, pet })
 })
 
-router.get("/testpage", (req, res) => {
-  const data = {
-    playerCurrency: 1000,
-    username: "USER",
-  }
-  const pet = {
-    lvl: 1,
-    xp: 10,
-    lvl1Story: "QWErty",
-    lvl2Story: "ASDfgh",
-    lvl3Story: "ZXCvbn",
-    lvl1Ability: "QWErty",
-    lvl2Ability: "ASDfgh",
-    lvl3Ability: "ZXCvbn",
-  }
-  res.render("testpage", { data, pet })
+router.get("/selectionpage", (req, res) => {
+  res.render("selectionpage")
 })
 
 module.exports = router;
