dced5e11c4e32cb7d8a4cb1c934b4d4e92e83e9e
Ivy Lee <hlee443@my.bcit.ca>
2022-05-20 16:52:58 -0700

    add rescaling to wordle page

--- public/js/rescale.js
+++ public/js/rescale.js
@@ -1,5 +1,6 @@
 // Page Rescaling
-function reScaleWhatever(backgroundElement) {
+const backgroundElement = document.querySelector(".background")
+function reScaleWhatever() {
     const scaleWidth = window.innerWidth / 1440
     const scaleHeight = window.innerHeight / 1024
     backgroundElement.style.transform = `translateX(-50%) translateY(-50%) scale(${scaleWidth, scaleHeight})`
@@ -8,3 +9,5 @@ function reScaleWhatever(backgroundElement) {
 window.addEventListener("resize", event => {
     reScaleWhatever()
 })
+
+reScaleWhatever()
